e91c191c856fb21586960bcf209a60b8ed35c83d
Sarus1997 <saharat.rus@gmail.com>
2023-07-24 08:45:34 +0700

    new

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--bootstrap5 css-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!--bootstrap5 js-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!--bootstrap5 Separate-->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+    <!-- sweetaert2 -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.all.min.js"></script>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
+    <link rel="stylesheet" href="src/styles.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <!--icon-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- font thai-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@300&display=swap" rel="stylesheet">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"></script>
+
+    <title>Information Technology</title>
+
+    <style>
+        body {
+            background-color: #ffffff;
+            font-family: 'Kanit', sans-serif;
+            font-size: 1rem;
+            margin: 0;
+            display: block;
+            overflow-x: hidden;
+            -webkit-text-size-adjust: 100%;
+            -webkit-tap-highlight-color: transparent;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="net-herder" class="header2" style=" max-width: 100%; height: 500px;">
+
+
+        <div class="container">
+            <font color="#FCB040" size="5">Pce-th</font>
+        </div>
+        <div style="text-align: center;">
+
+            <script>
+                VANTA.NET({
+                    el: "#net-herder",
+                    mouseControls: true,
+                    touchControls: true,
+                    gyroControls: false,
+                    minHeight: 200.00,
+                    minWidth: 200.00,
+                    scale: 1.00,
+                    scaleMobile: 1.00,
+                    color: 0x823fff,
+                    backgroundColor: 0x26232d,
+                    scaleMobile: 1.00,
+                    points: 12.00,
+                    spacing: 18.00
+                })
+            </script>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <div class="container" style="text-align: left;">
+                <h1 style="color:#ffffff">Information Technology</h1>
+                <h3 style="color:#ffffff">แผนกเทคโนโลยีสารสนเทศ</h3>
+            </div>
+        </div>
+    </div>
+
+    <br>
+    <div class="container" style="text-align: center;">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+
+                </div>
+                <div class="col-md">
+                <h3>การสรุปและติดตามงานแผนกไอที</h3>
+                <hr style="height: 0.5px; background: teal; margin: 20px 0; box-shadow: 0px 0px 4px 2px rgba(204,204,204,1);">
+                </div>
+                <div class="col-md-3">
+
+                </div>
+            </div>
+        </div>
+
+        <a href="/" type="button" class="btn btn-outline-success">สรุปแดชบอร์ด(โปรเจคไอที)</a>
+        <a href="/table" type="button" class="btn btn-outline-primary">การติดตามงานทางด้านไอที</a>
+
+
+    </div><br>
+
+    <div class="container-fluid">
+        <iframe src="https://view.monday.com/embed/4164184865-c446a29e1b32534c831ae77de313bd7c?r=use1" width=100% height=1000 style="border: 1; box-shadow: 5px 5px 56px 0px  rgba(204,204,204,1);"></iframe>
+    </div>
+
+    <footer class="bg-dark text-center text-white">
+        <div class="text-center py-4" style="background-color: #434344;">
+            © 2023 Copyright: <a class="text-reset fw-bold" href="https://www.pce-th.com/">
+                <font color="#FCB040">Pce-th.com</font>
+            </a>
+        </div>
+    </footer>
+</body>
+
+</html>
